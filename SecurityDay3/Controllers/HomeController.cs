e909eabddf15689f8fa21c5f7dffbb1e6972b060
Byul Kim <caxbelle@gmail.com>
2023-01-25 14:45:01 -0800

    Add project files.

---
+++ SecurityDay3/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecurityDay3.Data;
+using SecurityDay3.Models;
+using SQLitePCL;
+using System.Diagnostics;
+
+namespace SecurityDay3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public ApplicationDbContext _context;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        // Home page shows list of items.
+        // Item price is set through the ViewBag.
+        public IActionResult Index()
+        {
+            return View("Index", "3.55|CAD");
+        }
+
+        // Home page shows list of items.
+        // Item price is set through the ViewBag.
+        public IActionResult Transactions()
+        {
+            DbSet<IPN> items = _context.IPNs;
+
+            return View(items);
+        }
+
+
+
+        // This method receives and stores
+        // the Paypal transaction details.
+        [HttpPost]
+        public JsonResult PaySuccess([FromBody] IPN ipn)
+        {
+            try
+            {
+                _context.IPNs.Add(ipn);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message);
+            }
+            return Json(ipn);
+        }
+
+        // Home page shows list of items.
+        // Item price is set through the ViewBag.
+        public IActionResult Confirmation(string confirmationId)
+        {
+            IPN transaction =
+            _context.IPNs.FirstOrDefault(t => t.paymentID == confirmationId);
+
+            //IPN transaction2 = new IPN
+            //{
+            //    paymentID = "PAYID-12345",
+            //    payerFirstName = "Byul",
+            //    amount = "3.55",
+            //    currency = "CAD",
+            //};
+
+            return View("Confirmation", transaction);
+        }
+
+    }
+}
---
+++ SecurityDay3/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SecurityDay3.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace SecurityDay3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<IPN> IPNs { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+               base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+
+}
